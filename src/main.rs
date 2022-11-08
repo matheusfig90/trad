79de8aa64100fa57127ee14472163fb42c8ceedf
Igor Grkavac <grkavac.igor@gmail.com>
2022-11-08 22:47:19 +0000

    Setup dev env (#8)
    - Adding github action to build, check format and clippy warnings, and test.
    - Fix clippy warnings
    - Separate GUI part to a separate file

--- src/main.rs
+++ src/main.rs
@@ -1,69 +1,10 @@
-use crossbeam_channel::{select, tick, unbounded, Receiver};
-use crossterm::{
-    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
-    execute,
-    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
-};
-use std::{io, thread, time::Duration};
-use tui::{
-    backend::CrosstermBackend,
-    layout::{Constraint, Direction, Layout},
-    widgets::{Block, Borders, Widget},
-    Terminal,
-};
+mod main_app;
 
-fn setup_ui_events() -> Receiver<Event> {
-    let (sender, receiver) = unbounded();
-    thread::spawn(move || loop {
-        sender.send(crossterm::event::read().unwrap()).unwrap();
-    });
-
-    receiver
-}
+use main_app::App;
+use std::io;
 
 fn main() -> Result<(), io::Error> {
-    // setup terminal
-    enable_raw_mode()?;
-    let mut stdout = io::stdout();
-    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
-    let backend = CrosstermBackend::new(stdout);
-    let mut terminal = Terminal::new(backend)?;
+    let mut app = App::new();
 
-    let ui_events_receiver = setup_ui_events();
-    terminal.draw(|f| {
-        let size = f.size();
-        let block = Block::default().title("Block").borders(Borders::ALL);
-        f.render_widget(block, size);
-    })?;
-
-    loop {
-        select! { recv(ui_events_receiver) -> message => {
-
-            match message.unwrap() {
-                Event::Key(key_event) => {
-                    if key_event.modifiers.is_empty() {
-                        match key_event.code {
-                            KeyCode::Char('q') => {
-                                break
-                            },
-                            _ => {},
-                        }
-                    }
-                },
-                _ => {},
-            }
-        }
-        }
-    }
-
-    // restore terminal
-    disable_raw_mode()?;
-    execute!(
-        terminal.backend_mut(),
-        LeaveAlternateScreen,
-        DisableMouseCapture
-    )?;
-    terminal.show_cursor()?;
-
-    Ok(())
+    app.run()
 }
