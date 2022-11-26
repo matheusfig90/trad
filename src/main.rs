026990dc867e928c2a4c4cdeca050dcb613a1778
Matheus Figueredo <matheus.figueredo@gmail.com>
2022-11-26 14:10:44 +0000

    Add base CLI (#9)
    The CLI should provide an easy way for users to execute quick actions (write a note, add a goal, etc).

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,48 @@
+mod handlers;
 mod main_app;
 
+use handlers::echo_handler::EchoHandler;
 use main_app::App;
 use std::io;
+use structopt::StructOpt;
+
+#[derive(Debug, Clone, StructOpt)]
+enum Command {
+    Echo {
+        #[structopt(short = "m", long = "message")]
+        message: String,
+    },
+}
+
+#[derive(Debug, Clone, StructOpt)]
+#[structopt(name = "Trad")]
+struct MainArgs {
+    #[structopt(subcommand)]
+    command: Option<Command>,
+
+    // -d or --debug, true if it is present
+    #[structopt(short = "d", long = "debug")]
+    debug: bool,
+}
 
 fn main() -> Result<(), io::Error> {
-    let mut app = App::new();
+    let args = MainArgs::from_args();
+    if args.debug {
+        println!("{:?}", args);
+    }
 
-    app.run()
+    if args.command.is_none() {
+        let mut app = App::new();
+
+        return app.run();
+    }
+
+    match args.command.unwrap() {
+        Command::Echo { message } => {
+            let handler = EchoHandler::new(message);
+            handler.run();
+
+            Ok(())
+        }
+    }
 }
